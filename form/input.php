5e220d54b901a61077c13b300c92605eb3b4823f
kazuki-imamaki <elm.fb.gradual@gmail.com>
2022-04-01 09:44:11 +0900

    問い合わせ画面入力,確認,送信

--- form/input.php
+++ form/input.php
@@ -1,4 +1,6 @@
 <?php
+// スーパーグローバル変数９種類
+// 連想配列になっている your_nameがkey 入力値がvalue
 
 if(!empty($_POST)){
   echo '<pre>';
@@ -6,8 +8,16 @@ if(!empty($_POST)){
   echo '</pre>';
 }
 
-// スーパーグローバル変数９種類
-// 連想配列になっている your_nameがkey 入力値がvalue
+// 入力、確認、完了　input.php confirm.php thanks.php 
+
+$pageFlag = 0;
+
+if(!empty($_POST['btn-confirm'])){
+  $pageFlag =1;
+}
+if(!empty($_POST['btn-submit'])){
+  $pageFlag =2;
+}
 
 ?>
 <!DOCTYPE html>
@@ -19,16 +29,38 @@ if(!empty($_POST)){
   <title>Document</title>
 </head>
 <body>
+
+
+
+<?php if($pageFlag === 1) :?>
+  <form method="POST" action="input.php">
+    氏名
+    <?php echo $_POST['your_name']?>
+    <br>
+    メールアドレス
+    <?php echo $_POST['email']?>
+    <br>
+    <input type="submit" name = "btn-submit" value="送信します">
+    <input type="hidden" name="your_name" value="<?php echo _POST['your_name']?>">
+    <input type="hidden" name="email" value="<?php echo _POST['email']?>">
+  </form>
+<?php endif; ?>
+
+<?php if($pageFlag === 2) :?>
+送信が完了しました
+<?php endif; ?>
+
+<?php if($pageFlag === 0) :?> 
   <form method="POST" action="input.php">
     氏名
     <input type ="text" name="your_name">
-
-    <input type="checkbox" name="sports[]" value="野球">野球
-    <input type="checkbox" name="sports[]" value="サッカー">サッカー
-    <input type="checkbox" name="sports[]" value="バスケ">バスケ
-    
-
-    <input type="submit" value="送信">
+    <br>
+    メールアドレス
+    <input type ="email" name="email">
+    <br>
+    <input type="submit" name = "btn-confirm" value="確認する">
   </form>
+  <?php endif; ?>
+
 </body>
 </html>
